896fcccf205a3b98cb5f6198132f0c7a4b122aea
Lil-Krish <kcbhatraju@gmail.com>
2023-05-01 22:17:08 -0400

    Updated computational system

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-# Random Crop Registration
-Detect if a pair of images are properly registered through the computation of optical flow. 
+# Random Crops Registration
+This system computes translational displacements between pairs of images without reliance on optical flow, and therefore is also capable of predicting if such pairs are registered or not.
 
-<sub><sup>*This is part of my contribution to a Capstone Research Project involving the generation of consistent 360-degree panoramas from unregistered/incompatible images.</sup></sub><br/>
+<sub><sup>*This is part of a Capstone Research Project titled **Generative and Convolutional Modeling for Visual Dataset Processing**.</sup></sub><br/>
 <sub><sup>**A portion of this project placed second in Biomedical Engineering/third in Robotics & Intelligent Machines at the Central Kentucky Regional Science Fair.</sup></sub>
-
-## How to Use
-1. `initialize.py` defines a model instance that is untrained (if no model exists at `model/checkpoint.txt`). By default, the model is trained for 75 epochs on the [Skyfinder Dataset](https://cs.valdosta.edu/~rpmihail/skyfinder/).
-2. Evaluation is accuracy-based. The entire training dataset is registered, and as such automatically categorizes the network's performance. Due to the testing dataset's inclusion of both registered/unregistered pairs, user input is currently required for the measurement of accuracy.
-3. "`P Accuracy`" is defined as "proper accuracy", and measures the accuracy of the network on image pairs without data truncation.
---
+++ src/dispnet.py
@@ -0,0 +1,196 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+import copy
+import glob
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import ImageFile, Image
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+from torch import nn, set_grad_enabled, cat, load, tensor
+from torch.utils.data import Dataset, DataLoader
+
+import torchvision.transforms.functional as TF
+from torchvision import transforms
+from torchvision.transforms import RandomCrop
+from torchvision.utils import make_grid
+
+IM_ORIG_SIZE = (125, 125)
+IM_CROP_SIZE = (100, 100)
+
+
+class Crop(Dataset):
+    def __init__(self, root):
+        transform = transforms.Compose([transforms.Grayscale(), transforms.Resize(IM_ORIG_SIZE), transforms.ToTensor(), transforms.GaussianBlur(5)])
+        self.imgs = [transform(Image.open(path)) for path in root]
+        self.cropped = []
+        self.paired = []
+        for i in range(0,len(self.imgs),2):
+            self.paired.append([self.imgs[i],self.imgs[i+1]])
+        
+        self.present = [transforms.Resize(IM_ORIG_SIZE)(Image.open(path)) for path in root]
+        self.bin = []
+        for i in range(0,len(self.present),2):
+            self.bin.append([self.present[i],self.present[i+1]])
+            
+        every = list(zip(self.paired,self.bin))
+        random.shuffle(every)
+        self.paired, self.bin = zip(*every)
+    
+    def __len__(self):
+        return len(self.paired)
+    
+    def __getitem__(self, i):
+        img1, img2 = self.paired[i]
+        cimg1, cimg2 = self.bin[i]
+        
+        firstcrop = list(RandomCrop.get_params(img1, IM_CROP_SIZE))
+        if firstcrop[0] > 10: firstcrop[0] = int((firstcrop[0])/25*10) # edge case handling [if firstcrop x is > 110 then secondcrop x can go up to > 110+15=125 (off of picture)]
+        if firstcrop[1] > 10: firstcrop[1] = int((firstcrop[1])/25*10) # edge case handling [if firstcrop x is > 110 then secondcrop x can go up to > 110+15=125 (off of picture)]
+        firstcrop = tuple(firstcrop)
+        
+        secondcrop = list(firstcrop)
+        xshift = random.randint(0,15)
+        yshift = random.randint(0,15)
+        secondcrop[0] = firstcrop[0] + yshift
+        secondcrop[1] = firstcrop[1] + xshift
+        secondcrop = tuple(secondcrop)
+        
+        im1 = TF.crop(img1, *firstcrop)
+        im2 = TF.crop(img2, *secondcrop)
+        
+        color1 = TF.crop(cimg1, *firstcrop)
+        color2 = TF.crop(cimg2, *secondcrop)
+        self.cropped.append([color1, color2])
+        
+        return (cat([im1,im2]), tensor([xshift, yshift])) # (imgs, labels)
+
+
+cams = glob.glob("cameras/registered/*/")
+trainroot = []
+for cam in cams:
+    one = glob.glob(f"{cam}/*.jpg")
+    random.shuffle(one)
+    trainroot += one
+
+shuffims = []
+for i in range(0,len(trainroot),2):
+    shuffims.append([trainroot[i],trainroot[i+1]])
+
+random.shuffle(shuffims)
+trainroot = []
+for tup in shuffims:
+    [trainroot.append(val) for val in tup]
+
+cams = glob.glob("cameras/testing/*/")
+testroot = []
+for cam in cams:
+    one = glob.glob(f"{cam}/*.jpg")
+    random.shuffle(one)
+    testroot += one
+
+shuffims = []
+for i in range(0,len(testroot),2):
+    shuffims.append([testroot[i],testroot[i+1]])
+
+random.shuffle(shuffims)
+testroot = []
+for tup in shuffims:
+    [testroot.append(val) for val in tup]
+
+trainset = Crop(trainroot)
+testset = Crop(testroot)
+
+
+class DispNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        set_grad_enabled(True)
+        
+        self.drop = nn.Dropout(p=0.2)
+        self.relu = nn.LeakyReLU(0.1)
+        self.conv1 = nn.Conv2d(2,300,5,stride=2)
+        self.conv2 = nn.Conv2d(300,400,5,stride=2)
+        self.conv3 = nn.Conv2d(400,500,5,stride=2)
+        self.avgpool = nn.AvgPool2d((3,3))
+        self.flatten = nn.Flatten()
+        self.dense = nn.Linear(4500,16)
+    
+    def forward(self, x):
+        x = self.relu(self.drop(self.conv1(x)))
+        x = self.relu(self.drop(self.conv2(x)))
+        x = self.relu(self.drop(self.conv3(x)))
+        x = self.avgpool(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        return x
+
+
+def evaluate(type, times=None):
+    match type.lower():
+        case "train": set, name = trainset, "R"
+        case "test": set, name = testset, "T"
+        case _: raise ValueError("Invalid evaluation type.")
+    
+    if times is None: times = len(set)
+    
+    xflow = DispNet()
+    xflow.load_state_dict(load("models/xflow.txt"))
+    
+    yflow = DispNet()
+    yflow.load_state_dict(load("models/yflow.txt"))
+    
+    summed = 0
+    for i in range(min(times,len(set))):
+        xflow.eval(), yflow.eval()
+        img, lab = set[i]
+        x_lab, y_lab = lab
+        
+        x_probs = xflow.forward(img[None,:])
+        y_probs = yflow.forward(img[None,:])
+        
+        _, x_pred = x_probs.topk(1, dim=1)
+        _, y_pred = y_probs.topk(1, dim=1)
+        
+        transform = transforms.ToTensor()
+        full1, full2 = [transform(im) for im in set.bin[i]]
+        pil1, pil2 = [im.resize((125,125)) for im in set.cropped[i]]
+        crop1, crop2 = transform(pil1), transform(pil2)
+        
+        comb = Image.new("RGB", (pil1.width+x_pred.item(), pil1.height+y_pred.item()))
+        comb.paste(pil1, (0,0))
+        comb.paste(pil2, (x_pred.item(),y_pred.item()))
+        
+        imgs = cat((full1.unsqueeze(0),full2.unsqueeze(0),crop1.unsqueeze(0),crop2.unsqueeze(0)))
+        imgs = np.transpose(make_grid(imgs,padding=True,normalize=True),(1,2,0))
+        
+        """plt.imshow(imgs)
+        plt.show()"""
+        
+        match type.lower():
+            case "train": orig = "(R)"
+            case "test": orig = f"({input('Original: ').upper()})"
+        
+        match orig:
+            case "(R)": state = (abs(x_pred.item()-x_lab.item()) <= 3 and abs(y_pred.item()-y_lab.item()) <= 3)
+            case "(U)": state = (abs(x_pred.item()-x_lab.item()) > 1 or abs(y_pred.item()-y_lab.item()) > 1)
+            case _: raise ValueError("Invalid registration type.")
+        
+        summed += state
+        results = glob.glob("results/dispnet/*/")
+        size = len(glob.glob(f"{results[2]}/*/"))
+        file = 2-(state)*((orig == "(R)")+1)
+        size = len(glob.glob(f"{results[file]}/*/"))
+        path = f"{results[file]}{orig[1]}{size+1}/"
+        
+        with open("logs/dispnet/evaluate_log.txt", "a") as log:
+            log.write(f"{name}{orig[1]}{size+1}. True: ({x_lab.item()}, {y_lab.item()}), Predicted: ({x_pred.item()}, {y_pred.item()}) {('(Failed)','(Passed)')[state]}\n")
+    
+    with open("logs/dispnet/evaluate_log.txt", "a") as log:
+        log.write(f"{name} Accuracy: {round(summed/times*100,2)}%\n")
+ 
+evaluate("train")
